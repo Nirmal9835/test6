4405dcc03ce375b4eefdc1193227220ec418b985
Nirmal9835 <kumarviky921@gmail.com>
2025-05-20 20:41:42 +0530

    Update Test6Application.java

--- src/main/java/com/test6/Test6Application.java
+++ src/main/java/com/test6/Test6Application.java
@@ -9,6 +9,7 @@ public class Test6Application {
 	public static void main(String[] args) {
         String name="mike"
         int x = 100; 
+        boolean ispresent=false
 		SpringApplication.run(Test6Application.class, args);
 	}
 
